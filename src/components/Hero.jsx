0fb0ca9873cf663f1a4a3d50a98dd2c9231120e9
Bob <bob.vaz@hotmail.fr>
2024-04-18 10:58:10 +0200

    15.14

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,28 @@ import "./Hero.css"
 
 const Hero = () => {
   return (
-    <div>
-        
+    <div className='hero'>
+        <header>
+            <div className="top-nav">
+                <div className="top-nav">
+                    <div className="container flex">
+                        <div className="burger">
+
+                        </div>
+                        <div className='left-nav flex'>
+                            <div className="flex desk-top">
+                                <h1>1XWIN</h1>
+                            </div>
+                        </div>
+                        <div className="burger"></div>
+                        <div className="right-nav flex"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="main-head">
+
+            </div>
+        </header>
     </div>
   )
 }
